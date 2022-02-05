7cc0ddf229738623fd4d13466909ee913939a6dc
shoravs <sharmasaurav142@gmail.com>
2022-02-05 20:24:37 +0530

    added factorial program

--- src/com/mathematical/Factorial.java
+++ src/com/mathematical/Factorial.java
@@ -1,4 +1,25 @@
 package com.mathematical;
 
 public class Factorial {
+
+    // iterative solution
+    public static int factorial(int no){
+        int fact = 1;
+        for(int i = 2; i <= no; i++)
+            fact = fact * i;
+        return fact;
+    }
+
+    // recursive solution
+    public static int factorialRecursive(int no) {
+        if (no == 0)
+            return 1;
+        return no * factorial(no - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Iterative factorial is: " + factorial(5));
+        System.out.println("recursive factorial is: " + factorialRecursive(6));
+    }
+
 }
